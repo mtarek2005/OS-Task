5c783a073c3e25be102da5a96a754d2c138e88dc
Mohamed Tarek <43273151+mtarek2005@users.noreply.github.com>
2024-04-16 20:46:13 +0200

    list

--- main.c
+++ main.c
@@ -1,15 +1,38 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
+#include <dirent.h>
 
-
+void list_print(char* path){
+    DIR *d;
+    //one file or dir from dir, from dirent.h
+    struct dirent *dir;
+    //open dir, from dirent.h
+    d = opendir(path);
+    if (d) {
+        //loop over entries from readdir, from dirent.h
+        while ((dir = readdir(d)) != NULL) {
+            printf("%s\n", dir->d_name);
+        }
+        //close dir, from dirent.h
+        closedir(d);
+    }
+    else{
+        printf("error in list dir\n");
+    }
+}
 
 void main(){
+    // setup files at beginning 
     FILE *go=fopen("go.txt","w");
     if(!go)printf("error in create file\n");
     else fclose(go);
-    mkdir("Downloads2",S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
+    mkdir("Downloads",S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
+    remove("Downloads/go.txt");
+
+    list_print(".");
     //change permissions, from sys/stat.h
     //                r owner,w owner,r group,r other
     if(chmod("go.txt",S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH))printf("error in chmod\n");
@@ -29,11 +52,13 @@ void main(){
     //change dir, from unistd.h
     //always call list and cwd again after
     if(chdir("Downloads"))printf("error in chdir\n");
-    //list();
+    printf("changed to ");
     //get current dir, from unistd.h
     char* path = getcwd(NULL,0);
     if(!path)printf("error in pwd\n");
     else printf("%s\n",path);
+    //list again
+    list_print(".");
 
     //symbolic link, from unistd.h
     //      original | shortcut
